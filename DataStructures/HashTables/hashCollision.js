c8c0d6c9029ab5fa3bce4bd42cbb20230d169b89
JMComstock <jasoncomstock11@gmail.com>
2022-08-18 20:33:58 -0500

    added hash table directory and hash Collisions

---
+++ DataStructures/HashTables/hashCollision.js
@@ -0,0 +1,24 @@
+let user = {
+    age: 54,
+    name: 'Robert',
+    magic: true,
+    scream: function() {
+        console.log('AHHHHHHH!');
+    }
+}
+
+// The more key:value pairs that get added into the object of user,
+// the better the chance of a collision, where as key:value pairs will
+// share the same space in memory, 
+// there 2 methods to deal with this problem,
+// 1. linked list...
+
+// O(n/k) k is the size of the hash table with linked lists
+// for O(n/k), the rule for BigO states you remove constants, so O(n/k) becomes O(n)
+user.age  // O(1) <-- this is a lookup, it is O(1) but it could be O(n) if there is multiple key value pairs in the memory space
+user.spell = 'abra cadabra'; // O(1)
+console.log(user);
+user.scream(); // O(1)
+
+// O(n/k)
+
